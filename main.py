1f9bcc99557b20dfd9ec09d198b479a3e7e9b128
NEO-ENCODES <alpha369b@gmail.com>
2025-02-08 13:26:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from bot import Bot
 
-Bot().run()
+class MyBot(Bot):
+    def run(self):
+        # Call start() without any additional parameters.
+        self.start()
+        self.idle()
+        self.stop()
+
+if __name__ == "__main__":
+    MyBot().run()
